3f8e78bfb1136af30be8aa2bb028ebaad6ed1ec7
Osita Igwe <84817870+osita-igwe@users.noreply.github.com>
2021-07-24 18:19:21 -0400

    Add audio recorder

--- main.js
+++ main.js
@@ -27,7 +27,9 @@ const audio = {
 // Functions that change the color of the keys clicked on
 
 const keyPlay = function(event) {
-  event.target.style.backgroundColor = '#baf9fe';
+  console.log("Style", event.target.style)
+  event.target.style.background = 'rgb(255,121,9)';
+  //event.target.style.background: 'linear-gradient(red, yellow, blue)';
   let keyNote = event.target.id || event.target.parentElement.id;
   audio[keyNote].play();
 }
@@ -55,6 +57,13 @@ let nextTwo = document.getElementById('second-next-line');
 let nextThree = document.getElementById('third-next-line');
 let startOver = document.getElementById('fourth-next-line');
 
+// These variables store the text that indicates progression through the lyrics/lines
+let lyric1 = document.getElementById('lyric-1');
+let lyric2 = document.getElementById('lyric-2');
+let lyric3 = document.getElementById('lyric-3');
+let lyric4 = document.getElementById('lyric-4');
+
+
 // This variable stores the '-END' lyric element
 let lastLyric = document.getElementById('column-optional');
 
@@ -63,11 +72,20 @@ nextTwo.hidden = true; //Displays "line 3" when visible
 nextThree.hidden = true; //Displays "line 4" when visible
 startOver.hidden= true; //Displays "Reset" when visible
 
+// These statements are "hiding" all the text that shows progression through the lyric/lines, but the first one
+lyric2.hidden = true;
+lyric3.hidden = true;
+lyric3.hidden = true;
+lyric4.hidden = true;
+
+
 // Event handler property and function for the first progress button
 nextOne.onclick = function(event) {
   //Hides and unhides elements
   event.target.hidden = true;
   nextTwo.hidden = false;
+  lyric1.hidden = true;
+  lyric2.hidden = false;
   //Changes notes to play
   document.getElementById('letter-note-five').innerHTML = 'D';
   document.getElementById('letter-note-six').innerHTML = 'C';
@@ -79,6 +97,8 @@ nextTwo.onclick = function(event) {
   //Hides and unhides elements
   event.target.hidden = true;
   nextThree.hidden = false;
+  lyric2.hidden = true;
+  lyric3.hidden = false;
   //Changes lyrics
   document.getElementById('word-five').innerHTML = 'DEAR';
   document.getElementById('word-six').innerHTML = 'FRI-';
@@ -86,7 +106,7 @@ nextTwo.onclick = function(event) {
   //Changes notes to play
   document.getElementById('letter-note-three').innerHTML = 'G';
   document.getElementById('letter-note-four').innerHTML = 'E';
-   document.getElementById('letter-note-five').innerHTML = 'C';
+   document.getElementById('letter-note-five').innerHTML = '<sup>^</sup>C';
   document.getElementById('letter-note-six').innerHTML = 'B';
 }
 
@@ -95,6 +115,8 @@ nextThree.onclick = function(event) {
   //Hides and unhides elements
   event.target.hidden = true;
   startOver.hidden = false;
+  lyric3.hidden = true;
+  lyric4.hidden = false;
   //Changes lyrics
   document.getElementById('word-one').innerHTML = 'HAP-';
   document.getElementById('word-two').innerHTML = 'PY';
@@ -117,6 +139,8 @@ startOver.onclick = function() {
   //Hides and unhides elements
   nextOne.hidden = false;
   startOver.hidden = true;
+  lyric4.hidden = true;
+  lyric1.hidden = false;
   //Changes lyrics
   document.getElementById('word-one').innerHTML = 'HAP-';
   document.getElementById('word-two').innerHTML = 'PY';
@@ -129,6 +153,49 @@ startOver.onclick = function() {
   document.getElementById('letter-note-two').innerHTML = 'G';
   document.getElementById('letter-note-three').innerHTML = 'A';
   document.getElementById('letter-note-four').innerHTML = 'G';
-  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('letter-note-five').innerHTML = '<sup>^</sup>C';
   document.getElementById('letter-note-six').innerHTML = 'B';
+}
+
+//Script for Audio Recording
+ const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
+
+    const start = () => mediaRecorder.start();
+
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const audio = new Audio(audioUrl);
+          const play = () => audio.play();
+          resolve({ audioBlob, audioUrl, play });
+        });
+
+        mediaRecorder.stop();
+      });
+
+    resolve({ start, stop });
+  });
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+
+const handleAction = async () => {
+  const recorder = await recordAudio();
+  const actionButton = document.getElementById('action');
+  actionButton.disabled = true;
+  recorder.start();
+  await sleep(40000);
+  const audio = await recorder.stop();
+  audio.play();
+  await sleep(3000);
+  actionButton.disabled = false;
 }
